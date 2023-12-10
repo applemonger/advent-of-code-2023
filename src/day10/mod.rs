0439e4cbe3bca7560da451bf225aa9d61bfcfe06
Joseph Nawrocki <jonawrocki@gmail.com>
2023-12-10 15:41:17 -0500

    day10, pt2 attempt

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -1,10 +1,14 @@
+use std::collections::HashSet;
+
 use aocd::*;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Tile {
     value: char,
     gates: u8, // up, left, right, down
-    connects_to_start: bool
+    connects_to_start: bool,
+    port_flag: bool,
+    starboard_flag: bool
 }
 
 impl Tile {
@@ -24,7 +28,9 @@ impl Tile {
         Tile {
             value: c,
             gates,
-            connects_to_start
+            connects_to_start,
+            port_flag: false,
+            starboard_flag: false
         }
     }
 
@@ -52,6 +58,14 @@ impl Grid {
 
     pub fn connect_to_loop(&mut self, position: (usize, usize)) {
         self.grid[position.0][position.1].connects_to_start = true;
+    }
+
+    pub fn flag_port(&mut self, position: (usize, usize)) {
+        self.grid[position.0][position.1].port_flag = true;
+    }
+
+    pub fn flag_starboard(&mut self, position: (usize, usize)) {
+        self.grid[position.0][position.1].starboard_flag = true;
     }
 
     pub fn delete_tile(&mut self, position: (usize, usize)) {
@@ -158,10 +172,171 @@ pub fn solution1() {
 
 #[aocd(2023, 10)]
 pub fn solution2() {
-    let lines: Vec<String> = input!()
+// Read in grid map
+    // Add in margin of . tiles around grid so we don't have to care about out-of-bounds indexing
+    let mut grid: Vec<Vec<Tile>> = input!()
         .split('\n')
-        .map(|s| s.to_string())
+        .map(|s| {
+            let mut row = Vec::<Tile>::new();
+            row.push(Tile::from_char('.')); // margin
+            for c in s.chars() {
+                row.push(Tile::from_char(c))
+            }
+            row.push(Tile::from_char('.')); // margin
+            row
+        })
         .collect();
 
-    println!("{:?}", lines);   
+    // Add margin rows to top and bottom
+    let mut margin_row = Vec::<Tile>::new();
+    let ncols = grid[0].len();
+    for _ in 0..ncols {
+        margin_row.push(Tile::from_char('.'));
+    }
+    grid.insert(0, margin_row.clone());
+    grid.push(margin_row.clone());
+
+    // Determine grid size
+    let nrows = grid.len() - 1;
+    let ncols = grid[0].len() - 1;
+    
+    // Find starting position
+    let mut position = (0, 0);
+    for i in 1..nrows {
+        for j in 1..ncols {
+            if grid[i][j].value == 'S' {
+                position = (i, j);
+            }
+        }
+    }
+
+    // Helper struct
+    let mut grid = Grid { grid };
+
+    // Traverse tiles
+    // Visualize the traversal as a ship moving through a sea :)
+    // It has a port and starboard; we will track the tiles on its port and starboard as we go
+    let mut port = HashSet::<(usize, usize)>::new();
+    let mut starboard = HashSet::<(usize, usize)>::new();
+    let mut main_loop_count = 1;
+    'traverse: loop {
+        // Check adjacent tiles for connectivity
+        // Current position
+        let i = position.0;
+        let j = position.1;
+        // Position of adjacent tiles
+        let positions = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)];
+        // Gates that those respective adjacent tiles can connect to
+        let from_gates: [u8; 4] = [0b1000, 0b0001, 0b0100, 0b0010];
+        // Iterate through adjacent tiles
+        for (pos, from_gate) in positions.iter().zip(from_gates.iter()) {
+            // Check if it connects to the current tile
+            let connects = grid.tile((i, j)).connects_to(&grid.tile(*pos), *from_gate);
+            // Check if it has been flagged as added to the main loop (connects to start)
+            let added_to_loop = grid.tile(*pos).connects_to_start;
+            // If it connects and hasn't been added, flag it as added and move our current position
+            if connects && !added_to_loop {
+                // Mark the port & starboard of new position
+                let (new_port, new_starboard) = if *pos == (i-1, j) {
+                    // Moving up
+                    ((i-1, j-1), (i-1, j+1))
+                } else if *pos == (i+1, j) {
+                    // Moving down
+                    ((i+1, j+1), (i+1, j-1))
+                } else if *pos == (i, j-1) {
+                    // Moving left
+                    ((i+1, j-1), (i-1, j-1))
+                } else {
+                    // Moving right
+                    ((i-1, j+1), (i+1, j+1))
+                };
+                let is_margin = (new_port.0 == 0) || (new_port.0 == nrows) || (new_port.1 == 0) || (new_port.1 == 1);
+                if !is_margin {
+                    port.insert(new_port);
+                }
+                let is_margin = (new_starboard.0 == 0) || (new_starboard.0 == nrows) || (new_starboard.1 == 0) || (new_starboard.1 == 1);
+                if !is_margin {
+                    starboard.insert(new_starboard);
+                }
+                position = *pos;
+                grid.connect_to_loop(*pos);
+                main_loop_count += 1;
+                continue 'traverse;
+            }
+        }
+        break 'traverse;
+    }
+
+    // Flag tiles for port & starboard
+    let mut port_count = 0;
+    let mut starboard_count = 0;
+    for pos in port {
+        if !grid.tile(pos).connects_to_start {
+            grid.flag_port(pos);
+            port_count += 1;
+        }
+    }
+    for pos in starboard {
+        if !grid.tile(pos).connects_to_start {
+            grid.flag_starboard(pos);
+            starboard_count += 1;
+        }
+    }
+
+    // Iterate through tiles. If an empty tile is adjacent to a port or starboard tile, flag them as well
+    'flagging: loop {
+        let mut added_flag = false;
+        for i in 1..nrows {
+            for j in 1..ncols {
+                if !grid.tile((i, j)).connects_to_start {
+                    let positions = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)];
+                    for pos in positions {
+                        if grid.tile(pos).port_flag == true && !grid.tile((i, j)).port_flag {
+                            grid.flag_port((i, j));
+                            added_flag = true;
+                        }
+                        if grid.tile(pos).starboard_flag == true && !grid.tile((i, j)).starboard_flag {
+                            grid.flag_starboard((i, j));
+                            added_flag = true;
+                        }
+                    }
+                }
+            }
+        }
+        if !added_flag {
+            break 'flagging;
+        }
+    }
+
+    // Display grid
+    let mut starboard_count = 0;
+    let mut port_count = 0;
+    for row in grid.grid {
+        let row_string: String = row
+            .iter()
+            .map(|s| {
+                if s.port_flag && s.starboard_flag {
+                    panic!("Should not be possible!");
+                }
+                if (s.port_flag || s.starboard_flag) && s.connects_to_start {
+                    panic!("Inner/outer tiles cannot be on the main loop!");
+                }
+                if s.port_flag {
+                    port_count += 1;
+                    '@'
+                } else if s.starboard_flag {
+                    starboard_count += 1;
+                    '#'
+                } else {
+                    s.value
+                }
+            })
+            .collect();
+
+        println!("{}", row_string);
+    }
+
+    // Submit solution
+    println!("Main loop count: {}", main_loop_count / 2);
+    println!("{} {}", port_count, starboard_count);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use adventofcode::*;
 
 fn main() {
-    day10::solution1();
+    day10::solution2();
 }
